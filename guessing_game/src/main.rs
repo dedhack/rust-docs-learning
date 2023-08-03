d94cfd2ec74263dca276d1d4dcc885508635574f
Izhar Bin Jumadi <eezhar@gmail.com>
2023-08-03 12:31:00 +0800

    updated comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,13 +13,16 @@ fn main() {
     // let apples = 5; // immutable
     // let mut bananas = 5; // mutable
 
-    
+    // new is an associated function of the String type
+    // associated functions are implemented on types, rather than on instances of types
+    // new function creates a new, empty string
     let mut guess = String::new();
 
     // &mut guess is a reference to guess that allows the function to modify guess
+    // references are immutable by default
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        .read_line(&mut guess) // read_line returns a value of type io::Result
+        .expect("Failed to read line"); // expect is a method of io::Result types
 
     println!("You guessed: {guess}");
 }
